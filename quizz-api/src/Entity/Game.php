f5c98628f80b776d8bd400f8dc958010c427521b
ABDELFADEIL <abdalfadeil@gmail.com>
2021-02-11 16:02:31 +0100

    modif

--- quizz-api/src/Entity/Game.php
+++ quizz-api/src/Entity/Game.php
@@ -41,7 +41,7 @@ class Game
      * @ORM\ManyToMany(targetEntity=Quizz::class)
      * @ORM\JoinTable(name="game_quizzes",
      *joinColumns={@ORM\JoinColumn(name="game_id", referencedColumnName="id")},
-     *inverseJoinColumns={@ORM\JoinColumn(name="quizz_id", referencedColumnName="id", unique=true)})
+     *inverseJoinColumns={@ORM\JoinColumn(name="quizz_id", referencedColumnName="id", unique=false)})
      * @Groups({"quizz"})
      */
     private $quizzes;
