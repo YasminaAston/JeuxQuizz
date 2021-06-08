8e6b039a147ec78a54c3b664707663ba6b1540f1
ABDELFADEIL <abdalfadeil@gmail.com>
2021-06-08 16:03:37 +0200

    demoTest prête

---
+++ demoTest/out/production/demoTest/App\/dto/UserDto.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace App\dto;
+
+
+class UserDto
+{
+
+    private $email;
+    private $password;
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+
+
+}
