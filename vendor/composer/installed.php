042f0b62fcd6ebd14ddf98ba3c8510016190c592
MaxGsprd <max.gaspard@outlook.fr>
2021-06-10 11:17:01 +0200

    add login & registration fragments & laouts

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '21eedb3b5e5058a409b7fe6a57e2eaf639170e88',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '21eedb3b5e5058a409b7fe6a57e2eaf639170e88',
+            'dev_requirement' => false,
+        ),
+    ),
+);
