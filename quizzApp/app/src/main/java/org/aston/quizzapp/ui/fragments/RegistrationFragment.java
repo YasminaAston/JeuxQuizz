87cfdc88e88096d64a8c3bcdab960b53968ba0f6
MaxGsprd <max.gaspard@outlook.fr>
2021-07-09 17:20:44 +0200

    modify registration layout and add viewBinding

--- quizzApp/app/src/main/java/org/aston/quizzapp/ui/fragments/RegistrationFragment.java
+++ quizzApp/app/src/main/java/org/aston/quizzapp/ui/fragments/RegistrationFragment.java
@@ -2,65 +2,58 @@ package org.aston.quizzapp.ui.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import org.aston.quizzapp.R;
+import org.aston.quizzapp.databinding.FragmentLoginBinding;
+import org.aston.quizzapp.databinding.FragmentRegistrationBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RegistrationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class RegistrationFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class RegistrationFragment extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentRegistrationBinding binding;
+    private Button registerMeBtn;
+    private TextView backToLoginLink;
 
     public RegistrationFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RegistrationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RegistrationFragment newInstance(String param1, String param2) {
-        RegistrationFragment fragment = new RegistrationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentRegistrationBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        registerMeBtn = binding.loginBtn;
+        registerMeBtn.setOnClickListener(this);
+
+        backToLoginLink = binding.backToLoginLinkTv;
+        backToLoginLink.setOnClickListener(this::goBackToLogin);
+        return view;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onClick(View view) {
+        String emailEt = binding.emailEt.getText().toString();
+        String password = binding.passwordEt.getText().toString();
+        String firstname = binding.firstnameEt.getText().toString();
+        String lastname = binding.lastnameEt.getText().toString();
+        String username = binding.usernameEt.getText().toString();
+        System.out.println(emailEt);
+        return;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false);
+    public void goBackToLogin(View view) {
+        Navigation.findNavController(view).navigate(R.id.login_fragment);
     }
 }
